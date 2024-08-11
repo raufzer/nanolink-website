ca5d9ba2c5804f0be38c7e453dc4ebb0fdbcbac9
raufzer <a_zerkhef@estin.dz>
2024-08-11 05:37:48 +0100

    Added Lisence Frame

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,8 +21,7 @@ class HomePage extends StatelessWidget {
             child: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/images/elements.png')
-                ),
+                    image: AssetImage('assets/images/elements.png')),
               ),
             ),
           ),
@@ -122,6 +121,68 @@ class HomePage extends StatelessWidget {
               ),
             ],
           ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 63,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF1A284E),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Column(
+                                children: [
+                                  Center(
+                                    child: Positioned(
+                                      child: RichText(
+                                        text: const TextSpan(
+                                          text: 'Copyright © NanoLink LLC  ',
+                                          style: TextStyle(
+                                            fontFamily: 'Poppins-Regular',
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xFF8290A1),
+                                            fontSize: 20,
+                                          ),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                              text:
+                                                  ' •  Terms  •  Privacy Policy  •  Accessibility  •',
+                                              style: TextStyle(
+                                                fontFamily: 'Poppins-Regular',
+                                                fontWeight: FontWeight.w400,
+                                                color: Color(0xFF8290A1),
+                                                fontSize: 18,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
